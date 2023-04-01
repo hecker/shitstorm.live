8871ee90566fd7ea802620e3ca23c08cfe63b370
Jan Hecker <23746655+janhecker@users.noreply.github.com>
2023-04-01 23:31:20 +0200

    Add GitHub button + image.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import Image from "next/image";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        src="/shit.png"
+        alt="It is shit."
+        width={300}
+        height={300}
+        className="rounded-full"
+      />
+      <button className="bg-gray-800 text-white px-4 py-2 rounded mt-4">
+        <FaGithub className="inline-block mr-2" />
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://github.com/hecker/shitstorm.live"
+        >
+          Star on GitHub
+        </Link>
+      </button>
+    </div>
+  );
+};
+
+export default Home;
